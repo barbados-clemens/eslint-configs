e7a9d3c5b5bc7546933c545c44cc3d425d750827
Caleb Ukle <caleb@nrwl.io>
2024-11-22 10:20:22 -0600

    maybe working now???

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,29 @@
 const nx = require('@nx/eslint-plugin');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tseslint = require('typescript-eslint');
 
+// Note: detected a parser other than @typescript-eslint/parser.
+// Make sure the parser is configured to forward "parserOptions.project" to @typescript-eslint/parser.
+/**
+ * @type {import('eslint').Linter.FlatConfig[]}
+ */
 module.exports = [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
+  ...tseslint.configs.stylisticTypeChecked.map((c) => ({
+    ...c,
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-empty-function': 'warn',
+    },
+  })),
   {
     ignores: ['**/dist'],
   },
